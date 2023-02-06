d9ea866156461abc6a17d2ad203e44265bedfb1b
hoseok <shsshs0125@naver.com>
2023-02-06 16:10:48 +0900

    BoardRespDto 생성, 글 목록 보기 query 문 작성, BoardRepositoryTest 완료

--- src/main/java/shop/mtcoding/blog/model/BoardRepository.java
+++ src/main/java/shop/mtcoding/blog/model/BoardRepository.java
@@ -5,19 +5,24 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import shop.mtcoding.blog.dto.board.BoardRespDto.BoardMainRespDto;
+
 @Mapper
 public interface BoardRepository {
-    public int insert(@Param("userId") int userId, @Param("title") String title,
-            @Param("content") String content);
+        public int insert(@Param("userId") int userId, @Param("title") String title,
+                        @Param("content") String content);
 
-    public int updateById(@Param("id") int id, @Param("title") String title,
-            @Param("content") String content);
+        public int updateById(@Param("id") int id, @Param("title") String title,
+                        @Param("content") String content);
 
-    public int deleteById(int id);
+        public int deleteById(int id);
 
-    public Board findById(int id);
+        public Board findById(int id);
 
-    public Board findByUserId(int userId);
+        public Board findByUserId(int userId);
 
-    public List<Board> findAll();
+        public List<Board> findAll();
+
+        public List<BoardMainRespDto> findAllWithUser();
+
 }
---
+++ src/test/java/shop/mtcoding/blog/model/BoardRepositoryTest.java
@@ -0,0 +1,34 @@
+package shop.mtcoding.blog.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import shop.mtcoding.blog.dto.board.BoardRespDto.BoardMainRespDto;
+
+@MybatisTest
+public class BoardRepositoryTest {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Test
+    public void findAllWithUser_test() throws Exception {
+        // given
+        ObjectMapper om = new ObjectMapper();
+
+        // when
+        List<BoardMainRespDto> boardMainRespDto = boardRepository.findAllWithUser();
+        String responseBody = om.writeValueAsString(boardMainRespDto); // json 변환.
+        System.out.println("테스트 : " + responseBody);
+
+        // then
+        assertThat(boardMainRespDto.get(5).getUsername()).isEqualTo("love");
+    }
+}
